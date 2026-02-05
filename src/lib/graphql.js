e111a2b8f1854b757641f2e33981ba8562e5efb3
SharadKumar123 <Sharad.Kumar@xlparts.com>
2026-02-05 16:35:33 +0530

    correct the url

--- src/lib/graphql.js
+++ src/lib/graphql.js
@@ -2,7 +2,7 @@ const GRAPHQL_ENDPOINT =
   import.meta.env.VITE_GRAPHQL_URL ||
   (import.meta.env && import.meta.env.DEV
     ? 'http://localhost:4000/graphql'
-    : 'https://tms-backend.onrender.com/graphql');
+    : 'https://tms-bakend.onrender.com/graphql');
 
 export async function gqlRequest(query, variables = {}, token) {
   const res = await fetch(GRAPHQL_ENDPOINT, {
